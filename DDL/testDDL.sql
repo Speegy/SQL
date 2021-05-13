608cacc2bb88401b8b7d313b7ee6ab345f9e03b3
Speegy <mcjakedude@gmail.com>
2021-05-13 09:28:51 -0600

    Sakila updates

--- DDL/testDDL.sql
+++ DDL/testDDL.sql
@@ -2,6 +2,8 @@ SHOW DATABASES;
 CREATE DATABASE game_store;
 USE game_store;
 
+-- TABLES --
+
 CREATE TABLE customer(
 	id int primary key not null auto_increment,
     name varchar(100) not null,
@@ -28,8 +30,53 @@ FOREIGN KEY (customer_id) REFERENCES customer(id),
 FOREIGN KEY (product_id) REFERENCES products(id)
 );
 
-SHOW DATABASES;
+-- Get data info --
+
+show databases;
 use game_store;
 describe customer;
 describe products;
 describe orders;
+
+-- INSERTING DATA --
+-- customers --
+insert into customer(name, email, password) values ("Jake", "Jake@email.com", "Password1");
+insert into customer(name, email, password) values ("Josh", "Josh@email.com", "Password2");
+insert into customer(name, email, password) values ("Esperanza", "Esperanza@email.com", "Password3");
+insert into customer(name, email, password) values ("Alan", "Alan@email.com", "Password4");
+insert into customer(name, email, password) values ("Kevin", "Kevin@email.com", "Password5");
+
+-- products -- 
+insert into products(name, isNew, stock, price) values ("Jak and Daxter", 1, 50, 19.99);
+insert into products(name, isNew, stock, price) values ("Crash Bandicoot", 0, 3, 9.99);
+insert into products(name, isNew, stock, price) values ("StarWars Battlefront", 1, 500, 29.99);
+insert into products(name, isNew, stock, price) values ("Grandtrismo", 1, 75, 14.99);
+insert into products(name, isNew, stock, price) values ("Dynasty Warriors", 0, 10, 12.99);
+
+-- orders -- 
+
+insert into orders(product_id, customer_id, quantity) values (5, 1, 1);
+insert into orders(product_id, customer_id, quantity) values (3, 1, 1);
+insert into orders(product_id, customer_id, quantity) values (1, 1, 1);
+insert into orders(product_id, customer_id, quantity) values (3, 3, 1);
+insert into orders(product_id, customer_id, quantity) values (3, 4, 1);
+
+
+-- Testing data input --
+select * from customer;
+select * from products;
+select * from orders where customer_id = 1;
+select c.name, c.email, p.name, p.price from orders o join customer c on (o.customer_id=c.id) join products p on (o.product_id=p.id);
+
+-- Won't delete Customer because Customer has an Order -- 
+delete from customer where customer.name = "Alan";
+delete from customer where customer.id = 1;
+-- Will delete Customer because Customer doesn't have an Order -- 
+delete from customer where customer.id = 5;
+
+-- Alter / Update entry -- 
+update customer set name = "Kameron", email = "Kameron@gmail.com" where customer.id = 2;
+update products set name = "Borderlands", isNew = 0, stock = 2, price = 19.99 where products.id = 2;
+
+
+
